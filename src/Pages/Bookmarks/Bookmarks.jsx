62fb887afbcbdb746a9fd692dd2ccbd85d45fbf8
Bijo Mathew Jose <73273702+JackRiddlers@users.noreply.github.com>
2023-01-08 00:01:27 +0530

    update

--- src/Pages/Bookmarks/Bookmarks.jsx
+++ src/Pages/Bookmarks/Bookmarks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Navbar from '../../Components/Navbar';
 import CardComponent from '../../Components/CardComponent';
 import { connect, } from 'react-redux';
